f24a140e0bc7dbe5fe38e83550e667e545e80ddb
minticc3 <mintic.ciclo3@gmail.com>
2022-09-04 16:21:24 -0400

    Actualizacion del ApiCiclo3

--- Ciclo3Reto/ApiCiclo3/admin.py
+++ Ciclo3Reto/ApiCiclo3/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,Transaction,Enterprise,Rol,Employee
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Transaction)
+admin.site.register(Enterprise)
+admin.site.register(Rol)
+admin.site.register(Employee)
