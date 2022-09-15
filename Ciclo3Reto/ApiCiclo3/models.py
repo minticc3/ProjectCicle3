a9770ab487356800bd17a24550882e109f786fae
Leonardo Madrigal <108647588+Lmadrigalo@users.noreply.github.com>
2022-09-15 11:04:23 -0400

    Actualizado por Leo

--- Ciclo3Reto/ApiCiclo3/models.py
+++ Ciclo3Reto/ApiCiclo3/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Profile(models.Model):
+class Profile(models.Model): #Kevin
     id_profile = models.IntegerField(primary_key=True)
     image=models.CharField(max_length=100)
     phone=models.CharField(max_length=15)
@@ -9,7 +9,7 @@ class Profile(models.Model):
     createdAT = models.DateField()
     updateAT = models.DateField()
 
-class Transaction(models.Model):
+class Transaction(models.Model): #Bibiana
     id_transaction = models.IntegerField(primary_key=True)
     concept = models.CharField(max_length=80)
     amount = models.FloatField(max_length=10)
@@ -18,7 +18,7 @@ class Transaction(models.Model):
     createdAT = models.DateField()
     updateAT = models.DateField()
 
-class Enterprise(models.Model):
+class Enterprise(models.Model): #Andrea & Keyla
     id_enterprise = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
     document = models.CharField(max_length=10)
@@ -29,12 +29,12 @@ class Enterprise(models.Model):
     createdAT = models.DateField()
     updateAT = models.DateField()
 
-class Rol(models.Model):
+class Rol(models.Model): #Realizado en grupo
     id_role = models.IntegerField(primary_key=True)
     name_role = models.CharField(max_length=10, default="Admin")
 
-class Employee(models.Model):
-    id_employe = models.IntegerField(primary_key=True)
+class Employee(models.Model): #Leo
+    id_employee = models.IntegerField(primary_key=True)
     email = models.CharField(max_length=50)
     profile = models.ForeignKey(Profile , on_delete=models.CASCADE)
     id_role = models.ForeignKey(Rol, on_delete=models.CASCADE)
--- Ciclo3Reto/ApiCiclo3/urls.py
+++ Ciclo3Reto/ApiCiclo3/urls.py
@@ -4,4 +4,8 @@ from .views import RolViews
 urlpatterns=[
     path('rol/',RolViews.as_view(), name="Get"),
     path('rol/<int:id>',RolViews.as_view(), name="Update"),
+
+    path('Employee/',EmployeeViews.as_view(), name="Get"),
+    path('Employee/<int:id>',EmployeeViews.as_view(), name="Update"),
+    
 ]
