244d9bfc10f8a29844fbb6bdc6fe89c9102b482d
Keyla Barrios Araque <keybarriosaraque@gmail.com>
2022-09-16 20:25:55 -0500

    Subiendo cambios en Enterprise

--- Ciclo3Reto/ApiCiclo3/views.py
+++ Ciclo3Reto/ApiCiclo3/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views import View
 from .models import Rol
 from .models import Employee
+from .models import Enterprise
+from .models import Profile
+from .models import Transaction
 from django.http.response import JsonResponse
 import json
 from django.utils.decorators import method_decorator
@@ -98,3 +101,70 @@ class EmployeeViews(View):
             message={"Response:":"Not delete Successfully.."}
         return JsonResponse(message)  
 
+class EnterpriseViews(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id_ent=0):
+        if (id_ent > 0):
+            ent=list(Enterprise.objects.filter(id_enterprise=id_ent).values())
+            if (len(ent)>0):
+                entResponse=ent[0]
+                data={'Enterprise ->':entResponse}
+            else:
+                data={'message': "Object not found"}
+        else:
+            ent=list(Enterprise.objects.values())
+            data={'listEnterprise':ent}       
+        return JsonResponse(data)         
+
+    def post(self, request):
+        data=json.loads(request.body)
+        try:
+            pro=Enterprise.objects.get(id_profile=data["id_profile"])
+            transaction = Transaction.objects.get(id_transaction=data["id_transaction"])
+            ent=Enterprise.objects.create(id_enterprise=data["id_enterprise"],name=data["name"],document=data["document"],phone=data["phone"],address=data["address"],id_profile=pro,id_transaction=transaction,createdAT=data["createdAT"],updateAT=data["updateAT"])
+            ent.save()
+            message={"Response:":"Updated Successfully.."}
+            return JsonResponse(data)
+        except Enterprise.DoesNotExist:
+            message={"Response:":"Not exist.."} 
+        except Enterprise.DoesNotExist:
+            message={"Response:":"Not exist.."}
+        return JsonResponse(message)     
+
+    def post(self, request):
+       data=json.loads(request.body)
+       Enterprise.objects.creae(id=data["id_enterprise"], name=data["name"], document=data["document"],phone=data["phone"],address=data["address"],id_profile=data["id_profile"],id_transaction=data["id_transaction"])
+       return JsonResponse(data)      
+
+    def put(self, request, id_ent):
+        data=json.loads(request.body)
+        ent=list(Enterprise.objects.filter(id_enterprise=id_ent).values())
+        if (len(ent)>0):
+            pro=Profile.objects.get(id_profile=data["id_profile"])
+            transaction = Transaction.objects.get(id_transaction=data["id_transaction"])
+            enterprise=Enterprise.objects.get(id_enterprise=id_ent)
+            enterprise.name=data['name']
+            enterprise.document=data['document']
+            ent.enterprise.phone=data['phone']
+            enterprise.address=data['address']
+            enterprise.id_profile=pro['id_profile']
+            enterprise.id_transaction=transaction['id_transaction']
+            enterprise.createAT=data['createAT']
+            enterprise.updateAT=data['createAT']
+            enterprise.save()
+            message={"Response:":"Updated Successfully.."}
+        else:
+            message={"Response:":"Not Successfully.."}
+        return JsonResponse(message)
+
+    def delete(self,request,id_ent):
+        ent=list(Enterprise.objects.filter(id_enterprise=id_ent).values())
+        if(len(ent)>0):
+            Enterprise.objects.filter(id_enterprise=id_ent).delete()
+            message={"Response:":"Delete Successfully.."}
+        else:
+            message={"Response:":"Not exist.."}
+        return JsonResponse(message)
